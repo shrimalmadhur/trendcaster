1b1c57f86f84513751568aa39dfb066cf1833188
Madhur Shrimal <madhur.shrimal@gmail.com>
2022-08-12 00:43:56 -0700

    disable prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import { FC, useContext } from 'react';
-import dynamic from 'next/dynamic';
-import styles from '@/styles/Home.module.css';
-import Trends from 'components/Trends';
-import axios from 'axios'
-import { MongoClient, ServerApiVersion } from 'mongodb'
+import { FC, useContext } from "react";
+import dynamic from "next/dynamic";
+import styles from "@/styles/Home.module.css";
+import Trends from "components/Trends";
+import axios from "axios";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 interface Props {
-  response: any
+  response: any;
 }
-const Home: FC<Props> = ( {response}) => {
+const Home: FC<Props> = ({ response }) => {
   return (
     <div className={styles.container}>
       <Trends data={response}></Trends>
@@ -18,58 +18,57 @@ const Home: FC<Props> = ( {response}) => {
 
 export async function getServerSideProps() {
   // Fetch data from external API
-  console.log("broooooo")
+  console.log("broooooo");
   const mongodbURL = process.env.NEXT_MONGODB_URI;
   if (!mongodbURL) {
-    return { props: { response: "" } }
+    return { props: { response: "" } };
   }
   const client = new MongoClient(mongodbURL, {
-      serverApi: ServerApiVersion.v1,
-  })
+    serverApi: ServerApiVersion.v1,
+  });
 
   client.connect((err) => {
-      if (err) {
-          console.error(err)
-          return { props: { response: "" } }
-      }
-  })
-  const db = client.db('farcaster')
+    if (err) {
+      console.error(err);
+      return { props: { response: "" } };
+    }
+  });
+  const db = client.db("farcaster");
 
   // Order by desc and last 10 days
   const profileCounts = await db
-    .collection('profiles_count')
+    .collection("profiles_count")
     .find({})
     .toArray()
     .catch(() => {
-        console.error('Error getting number of profiles from MongoDB')
-        return null
-    })
+      console.error("Error getting number of profiles from MongoDB");
+      return null;
+    });
 
-  if (!profileCounts){
-    return { props: { response: "" } }
+  if (!profileCounts) {
+    return { props: { response: "" } };
   }
-  const labels = []
-  const countArray = []
-  for(let eachCount of profileCounts) {
-    labels.push((new Date(eachCount.time)).toLocaleDateString("eb-US"));
-    countArray.push(eachCount.count)
+  const labels = [];
+  const countArray = [];
+  for (let eachCount of profileCounts) {
+    labels.push(new Date(eachCount.time).toLocaleDateString("eb-US"));
+    countArray.push(eachCount.count);
   }
   const data = {
-      labels,
-      datasets: [
-        {
-          label: 'Profiles',
-          data: countArray,
-          borderColor: 'rgb(255, 99, 132)',
-          backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        }
-      ],
+    labels,
+    datasets: [
+      {
+        label: "Profiles",
+        data: countArray,
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
   };
   // console.log(data)
   // Pass data to the page via props
-  return { props: { response: data } }
+  return { props: { response: data } };
 }
-
 
 // export default Home
 export default dynamic(() => Promise.resolve(Home), { ssr: false });
