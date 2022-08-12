c0cfa847ae16c53685b5833e91eef0ed5a9f88ce
Madhur Shrimal <madhur.shrimal@gmail.com>
2022-08-11 21:43:52 -0700

    counts and word counts

--- indexer/index.js
+++ indexer/index.js
@@ -3,6 +3,8 @@ const got = require('got')
 const cron = require('node-cron')
 const { providers, Contract, utils } = require('ethers')
 const { MongoClient, ServerApiVersion } = require('mongodb')
+const { removeStopwords } = require('stopword')
+const { keys } = require('./registry-abi.js')
 
 console.log(process.env.MONGODB_URI)
 const client = new MongoClient(process.env.MONGODB_URI, {
@@ -47,6 +49,7 @@ async function indexCasts() {
     let profilesIndexed = 0
     const profiles = await db
         .collection('profiles')
+        // .find({"merkleRoot": "0x17cc2df1a869ed2031cb0466debe6855088366792dc637c2a33622b95660138c"})
         .find({})
         .toArray()
         .catch(() => {
@@ -85,11 +88,99 @@ async function indexCasts() {
     }
     await newCollection.rename('casts')
 
+
+    const countData = {
+        count: allCasts.length,
+        time: Date.now()
+    }
+
+    const castsCountCollection = db.collection('casts_count');
+    await castsCountCollection
+        .insertOne(countData)
+        .then(() => console.log("casts count data inserted"))
+        .catch((err) => {
+            console.log(`Error saving cast count data ${err.message}`)
+        });
+
+    const date = Date.now() - 24*60*60*1000
+
+    // filters - date > 24 hrs
+    // filter our recast:farcaster://
+    // remove stop words
+
+    const re = /\s+/;
+    const filteredCast = allCasts
+        .filter(cast => cast.body.publishedAt > date)
+        .filter(cast => !cast.body.data.text.startsWith("recast:farcaster://"))
+        .filter(cast => !cast.body.data.text.startsWith("delete:farcaster://"))
+        .map(cast => removeStopwords(cast.body.data.text.split(re)))
+
+    console.log(`last 24 hrs filtered casts count is ${filteredCast.length}`);
+
+    // const re = /\s+/;
+    const wordMap = {};
+    const wordCount = []
+    for (const castArr of filteredCast) {
+        // console.log(castArr)
+        for (let word of castArr) {
+            // console.log(word)
+            word = word.toLowerCase()
+            // TODO: remove only punctuations
+            // TODO: remove punctuations from start and end
+            if (word.startsWith("@") || word.startsWith("https://") || word.startsWith("http://") || !isNaN(word)) {
+                continue;
+            }
+            // console.log(wordMap[word])
+            if (!wordMap[word]) {
+                wordMap[word] = 0
+                // console.log(wordMap[word])
+            }
+            wordMap[word] = wordMap[word] + 1;
+        }
+    }
+
+    // console.log(wordMap)
+
+    // remove small counts 
+    const removeCount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    for(let word in wordMap) {
+        if (removeCount.includes(wordMap[word])){
+            continue;
+        }
+        wordCount.push({word: word, count: wordMap[word]});
+    }
+
+    const oldWC = db.collection('word_count')
+    const newWC= db.collection('word_count_temp')
+
+    // If the temp table already exists, drop it
+    try {
+        await newWC.drop()
+    } catch {}
+
+    await newWC.createIndex({ count: 1 })
+
+    await newWC.insertMany(wordCount).catch((err) => {
+        console.log(`Error saving word count to MongoDB.`, err.message)
+    })
+
+    // Replace existing collection with new casts
+    try {
+        await oldWC.drop()
+    } catch (err) {
+        console.log('Error dropping collection.', err.codeName)
+    }
+    await newWC.rename('word_count')
+
+    // console.log(wordCount)
+
     const endTime = Date.now()
     const secondsTaken = (endTime - startTime) / 1000
+
     console.log(
         `Saved ${allCasts.length} casts from ${profilesIndexed} profiles in ${secondsTaken} seconds`
     )
+    console.log('done')
 }
 
 async function indexProfiles() {
@@ -185,6 +276,19 @@ async function indexProfiles() {
     }
     await newCollection.rename('profiles')
 
+    const countData = {
+        count: profilesIndexed,
+        time: Date.now()
+    }
+    const profileCountCollection = db.collection('profiles_count');
+    await profileCountCollection
+        .insertOne(countData)
+        .then(() => console.log("profile count data inserted"))
+        .catch((err) => {
+            console.log(`Error saving count data ${err.message}`)
+        })
+    
+
     const endTime = Date.now()
     const secondsTaken = (endTime - startTime) / 1000
     console.log(
@@ -192,7 +296,9 @@ async function indexProfiles() {
     )
 }
 
-indexProfiles()
+// indexProfiles()
+
+indexCasts()
 
 // // Run job every 2 hours
 // cron.schedule('0 */2 * * *', () => {
