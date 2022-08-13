8d975a4cc49aba109f37992da72a7bd59bd56d11
Madhur Shrimal <madhur.shrimal@gmail.com>
2022-08-12 21:23:41 -0700

    add csp header for analytics

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self' vitals.vercel-insights.com;
+`
+
+// You can choose which headers to add to the list
+// after learning more below.
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  }
+]
+
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/vitals',
+        headers: securityHeaders,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
