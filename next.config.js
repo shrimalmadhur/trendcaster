4aadf7dd15ec3cd2f4f016394b6d7c2ab7afc028
Madhur Shrimal <madhur.shrimal@gmail.com>
2022-08-18 00:31:51 -0700

    add wallet connection and data

--- next.config.js
+++ next.config.js
@@ -16,16 +16,7 @@ const securityHeaders = [
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/:path*',
-        headers: securityHeaders,
-      },
-    ]
-  },
+  swcMinify: true
 };
 
 module.exports = nextConfig;
